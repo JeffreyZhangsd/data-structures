e6e431bee59a691f5fa89c21ba08b36b1a2c439f
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 16:31:00 -0700

    pseudoclassical functionality completed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //use keyword new and this
+  this.first = 0;
+  this.index = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+  return;
+};
+
+Queue.prototype.dequeue = function(value) {
+  var firstQueue = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return firstQueue;
+};
+
+Queue.prototype.size = function() {
+  if (this.first > this.index) {
+    return 0;
+  }
+  if (this.index > 0) {
+    return this.index - this.first;
+  } else {
+    return 0;
+  }
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //use keyword new and this
+  this.index = 0;
+  this.storage = {};
+
+  //don't declare instance explicitly or ret urn it explicitly
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+  return;
+};
+
+Stack.prototype.pop = function(value) {
+  this.index--;
+  var popped = this.storage[this.index];
+  delete this.storage[this.index];
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  if (this.index > 0) {
+    return this.index--;
+  } else {
+    return 0;
+  }
+};
 
