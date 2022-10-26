3e818ab49e9fa1cdedca9f5a704d385788cded85
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 14:17:56 -0700

    passes test cases for queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,36 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var first = 0; //first index
+  var i = 0; //numeric key
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //starting at index 0, should add into queue
+    storage[i] = value;
+    i++;
   };
 
   someInstance.dequeue = function() {
+    //should dequeue from earliest index
+    //looking at first key
+    var firstQueue = storage[first]
+    first++;
+    return firstQueue;
   };
 
   someInstance.size = function() {
+    //should get size of queue
+    if (first > i) {
+      return 0;
+    }
+
+    if (i > 0) {
+      return i - first;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,32 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var i = 0; //index
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[i] = value;
+    //should add value to end of object
+    if (i in storage) {
+      someInstance[i] = value;
+      i++
+    }
+    return;
   };
 
   someInstance.pop = function() {
+    //should remove most recent value from end of object
+    i--;
+    return storage[i];
   };
 
   someInstance.size = function() {
+    //should get size of stack
+    if (i >= 0) {
+      return i--;
+    } else {
+      return 0;
+    }
   };
-
   return someInstance;
 };
