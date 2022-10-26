9f86db55b45d7a08317365a4c1bbfa5f36d37715
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 16:39:06 -0700

    completed functionality of es6 instantiation

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.first = 0;
+    this.index = 0;
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.storage[this.index] = value;
+    this.index++;
+    return;
+  }
+
+  dequeue(value) {
+    var firstQueue = this.storage[this.first];
+    delete this.storage[this.first];
+    this.first++;
+    return firstQueue;
+  }
+
+  size() {
+    if (this.first > this.index) {
+      return 0;
+    }
+    if (this.index > 0) {
+      return this.index - this.first;
+    } else {
+      return 0;
+    }
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    //constructor builds current instance of object
+    this.index = 0;
+    this.storage = {};
   }
+  //functions within the same class of Stack
 
+  push(value) {
+    this.storage[this.index] = value;
+    this.index++;
+    return;
+  };
+
+  pop(value) {
+    this.index--;
+    var popped = this.storage[this.index];
+    delete this.storage[this.index];
+    return popped;
+  };
+
+  size() {
+    if (this.index > 0) {
+      return this.index--;
+    } else {
+      return 0;
+    }
+  };
 }
