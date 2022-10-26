73547cd1ebba38d192b728e150bc2fbe323df7fd
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 15:46:52 -0700

    functional-shared method complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.index = 0;
+  instance.storage = {};
+  //use thi s
+  //use _.extend
+  _.extend(instance, stackMethods);
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {}
+
+//should push value into end
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+  return;
+};
+
+//should pop out value at end
+stackMethods.pop = function(value) {
+  this.index--;
+  var popped = this.storage[this.index];
 
 
+  delete this.storage[this.index];
+
+  return popped;
+};
+
+//should get stack size
+stackMethods.size = function() {
+  if (this.index > 0) {
+    return this.index--;
+  } else {
+    return 0;
+  }
+};
