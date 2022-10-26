baf7b14a512ce111bc646c3636ee6fb91c191060
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 16:16:12 -0700

    completed prototypal functionality

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //use object.create
+  var instance = Object.create(queueMethods);
+  instance.first = 0;
+  instance.index = 0;
+  instance.storage = {};
+  //don't need to extend because we've created a new instance object that will contain the queuemethods
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+  return;
+}
+
+queueMethods.dequeue = function(value) {
+  var firstQueue = this.storage[this.first];
+  delete this.storage[this.first];
+  this.first++;
+  return firstQueue;
+}
+
+queueMethods.size = function() {
+  if (this.first > this.index) {
+    return 0;
+  }
+  if (this.index > 0) {
+    return this.index - this.first;
+  } else {
+    return 0;
+  }
+}
+
+
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.index = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++
+  return;
+}
+
+stackMethods.pop = function(value) {
+  this.index--;
+  var popped = this.storage[this.index];
+  delete this.storage[this.index];
+  return popped;
+}
+
+stackMethods.size = function() {
+  if (this.index > 0) {
+    return this.index--;
+  } else {
+    return 0;
+  }
+}
 
