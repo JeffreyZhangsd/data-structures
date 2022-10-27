5b1b9fe2164ace6f4d9a44d36ca359309b7fe9c0
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-26 18:46:49 -0700

    started pair portion of sprint, got some ideas down for addToTail and removedHead, and also wrote contains but haven't tested it much yet

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  //linkedlist keeps track of order
+  //the head points to the next "node" in the linked list, which points to it's next node
+  //until it reaches the tail which doesn't point to any node
+  // { '12': 6, '6': 71, '71': 26, '26': null }
+  // { key: next key }
 
   list.addToTail = function(value) {
+    //our last ideas weereee uhhh
+    //we get a new value
+    //we set the key value of our old tail to be our new value
+    //we set the new tail to be our given value
+    //boom profit
   };
 
   list.removeHead = function() {
+    //
   };
 
   list.contains = function(target) {
+    //find target using key
+    //return true
+    for (var key in list) {
+      if (list[key] === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
