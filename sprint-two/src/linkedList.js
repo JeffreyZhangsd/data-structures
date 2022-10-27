ede6888bf98b82350b0bae54d49a90c0ff47c111
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-27 16:49:18 -0700

    linked list completed, passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,37 +1,64 @@
 var LinkedList = function() {
   var list = {};
+  //debugger;
   list.head = null;
   list.tail = null;
   //linkedlist keeps track of order
   //the head points to the next "node" in the linked list, which points to it's next node
   //until it reaches the tail which doesn't point to any node
-  // { '12': 6, '6': 71, '71': 26, '26': null }
-  // { key: next key }
+  // { value: actual value, next: actual next node || null }
 
   list.addToTail = function(value) {
-    //our last ideas weereee uhhh
-    //we get a new value
-    //we set the key value of our old tail to be our new value
-    //we set the new tail to be our given value
-    //boom profit
+    //create new Node(value)
+    //list.tail.next = new Node
+    //list.tail = new Node
+    var newTail = new Node(value);
+    if (list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    } else {
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
-    //
+    //var past = list.head
+    //update list.head
+    //return past
+    var past = list.head.value;
+    list.head = list.head.next;
+    return past;
   };
 
   list.contains = function(target) {
-    //find target using key
-    //return true
-    for (var key in list) {
-      if (list[key] === target) {
+    //search current Node
+    //if value === target
+      //return true
+    //otherwise if current Node has next value
+      //call contains on next Node
+    //otherwise
+      //return false
+
+      //checkCurrentNode()
+    if (this.value === target) {
+      return true;
+    }
+
+    var checkCurrentNode = function(node) {
+      if (node.value === target) {
         return true;
       }
+      if (node.next !== null) {
+        return checkCurrentNode(node.next);
+      }
+      return false;
     }
-    return false;
+    return checkCurrentNode(list.head);
   };
-
+  //RETURN LIST
   return list;
+
 };
 
 var Node = function(value) {
@@ -45,4 +72,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+seems logarithmic
+
  */
