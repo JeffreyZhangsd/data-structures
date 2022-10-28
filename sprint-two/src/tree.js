9bb4c90eef97d0fbeeb4d4ae2f39e471b1ac1d50
Jeffrey Zhang <jzhang22la@gmail.com>
2022-10-27 17:22:50 -0700

    completed tree data structure tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,52 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  //extend treeMethods
+  newTree.children = [];  //array containing a number of subtrees
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
-
+//takes any value, sets that as the target of a node and adds that node as a child of the tree
 treeMethods.addChild = function(value) {
+  //create a tree object
+    //with value and children = []
+  //push tree object into current (this) .children
+  var newestTree = new Tree(value);
+  this.children.push(newestTree);
 };
 
+//reads through nodes and returns boolean if target is found
 treeMethods.contains = function(target) {
+  //if this.value === target
+    //return true;
+  //for each child
+    //checkCurrentTree(TreeNode)
+  var checkCurrentTree = function(tree) {
+    //for each child, check child value and THEN check their children
+    if (this.value === target) {
+      return true;
+    }
+    //if no children, do this
+    if (this.children.length === 0) {
+      return false;
+    }
+    var foundBelow = false;
+    this.children.forEach(function(child) {
+      //checkCurrentTree(child) --> returns boolean
+      //foundBelow = foundBelow || checkCurrentTree(child)
+      var checked = checkCurrentTree.call(child);
+      foundBelow = foundBelow || checked;
+    });
+    return foundBelow;
+  }
+  return checkCurrentTree.call(this);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+linear time complexity
+
  */
